6ab8f76de8463a7e08b31bc21a0b954246bab404
edgar sarkisian <gesarki@gmail.com>
2018-09-15 12:03:34 -0400

    added js function to pull current state of the table into a 2d array

--- gameOfLife.js
+++ gameOfLife.js
@@ -5,6 +5,19 @@ var table = document.getElementById("boardTable");
 var NUMROWS = 15;
 var NUMCOLS = 15;
 
+// the 2d array that will be used to hold the states of every cell
+// and determine it's next generation
+// must initialize with all 0s
+var cells = [];
+var i=0;
+var j=0;
+for (i ;i<NUMROWS; i++) {
+    cells[i] = [];
+    for (j=0; j<NUMCOLS; j++) {
+        cells[i][j] = 0;
+    }
+}
+
 // build the board
 function bodyOnLoad() {
     for (var i = 0; i < NUMROWS; i++) {
@@ -24,5 +37,19 @@ function toggleAliveClass(cell) {
         cell.className = classes.replace( /(?:^|\s)alive(?!\S)/g , '' );
     } else {
         cell.className += "alive";
+    }
+}
+
+// pull the current state of the board to a 2d array of ints
+// with 0 representing dead and 1 representing alive
+function pullTo2dArray() {
+    // get all cells with class "alive"
+    var aliveCells = table.querySelectorAll('td[class^="alive"]');
+
+    // set those cells to 1 in the 2d array
+    for (var i = 0; i < aliveCells.length; i++) {
+        var y = aliveCells[i].parentNode.rowIndex;
+        var x = aliveCells[i].cellIndex;
+        cells[y][x] = 1;
     }
 }
