2bcc781b6293826fd3c039e369e6172aedc5a9ba
edgar sarkisian <gesarki@gmail.com>
2018-09-15 03:43:34 -0400

    added funcionality to make alive cells black, based on their tag classes

--- gameOfLife.js
+++ gameOfLife.js
@@ -5,13 +5,24 @@ var table = document.getElementById("boardTable");
 var NUMROWS = 15;
 var NUMCOLS = 15;
 
+// build the board
 function bodyOnLoad() {
     for (var i = 0; i < NUMROWS; i++) {
         var newRow = table.insertRow();
 
         for (var j = 0; j < NUMCOLS; j++) {
-            newRow.insertCell();
+            var newCell = newRow.insertCell();
+            newCell.onclick = function() {toggleAliveClass(this);};
         }
     }
 }
 
+// toggles the "alive" class of the cell which controls its color
+function toggleAliveClass(cell) {
+    var classes = cell.className;
+    if ( classes.match(/(?:^|\s)alive(?!\S)/) ){
+        cell.className = classes.replace( /(?:^|\s)alive(?!\S)/g , '' );
+    } else {
+        cell.className += "alive";
+    }
+}
